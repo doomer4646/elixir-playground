cacdea2e6c3cd5f588c081d2ffdacb16c6e6f0c4
doomer4646 <doomergtd@gmail.com>
2025-09-12 22:48:55 +0900

    ミニ関数の作成

--- hello/lib/hello.ex
+++ hello/lib/hello.ex
@@ -1,22 +1,50 @@
 defmodule Hello do
   @moduledoc """
-  Documentation for `Hello`.
+  パターンマッチで戻り値をさばく練習モジュール
   """
-
   @doc """
-  Hello world.
+
 
   ## Examples
 
-      iex> Hello.hello()
-      :world
-
+  iex -S mix
+  iex(3)>  Hello.parse_integer("12x")
+  {:error, :invalid_integer}
+  iex(4)> Hello.parse_integer("12")
   """
-  def hello do
-    :world
+  # 例１：文字列を整数に変換
+  def parse_integer(str) when is_binary(str) do
+    case Integer.parse(str) do
+      {int, ""} -> {:ok, int}
+      _ -> {:error, :invalid_integer}
+    end
   end
 
-  def greet(name) do
-    "Hello, #{name}!"
+  # 例２：年齢バリデーション
+  def validate_age(age) when is_integer(age) and age >= 0 do
+    if age >= 20, do: {:ok, :adult}, else: {:ok, :minor}
   end
+
+  # 例３：ユーザー検索っぽいもの
+  def fetch_user(id) when is_integer(id) and id > 0 do
+    if id == 42 do
+      {:ok, %{id: 42, name: "User42"}}
+    else
+      {:error, :not_found}
+    end
+  end
+
+  def fetch_user(_), do: {:error, :invalid_id}
+
+  # 例４：蒸気を組み合わせるユースケース
+  def show_user(id_str) when is_binary(id_str) do
+    with {:ok, id} <- parse_integer(id_str),
+         {:ok, user} <- fetch_user(id) do
+      {:ok, "ID=#{user.id}, Name=#{user.name}"}
+    else
+      {:error, reason} -> {:error, reason}
+    end
+  end
+
+  def show_user(_), do: {:error, :invalid_id}
 end
